061051afb8c001e92661ef77c8cc5344790928a7
Hoshiiro <pidiyarto@gmail.com>
2023-05-31 14:54:22 +0700

    mekanisme pacman escape
    yang perlu diperbaiki
    -ukuran layar di game
    -gambar tidak mau ke load mungkin karena salah ngambil file location
    -belum buat pintu keluarnya

---
+++ Pacman_Escape/src/main/java/com/mycompany/pacman_escape/Pacman_Escape.java
@@ -0,0 +1,20 @@
+package com.mycompany.pacman_escape;
+
+import javax.swing.JFrame;
+public class Pacman_Escape extends JFrame {
+    
+        public Pacman_Escape () {
+		add(new Model());
+	}
+	
+	
+	public static void main(String[] args) {
+		Pacman_Escape pac = new Pacman_Escape();
+		pac.setVisible(true);
+		pac.setTitle("Pacman");
+		pac.setSize(380,420);
+		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pac.setLocationRelativeTo(null);
+		
+	}
+}
