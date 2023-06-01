609007aedc62f97e2a5a8fcc1bcd5dca0bcc7c1d
Muhammad robah <86682953+Daehoo0@users.noreply.github.com>
2023-06-01 20:31:09 +0700

    wal
    wal

--- Pacman_Escape/src/main/java/com/mycompany/pacman_escape/Pacman_Escape.java
+++ Pacman_Escape/src/main/java/com/mycompany/pacman_escape/Pacman_Escape.java
@@ -11,12 +11,16 @@ public class Pacman_Escape extends JFrame {
 	
 	
 	public static void main(String[] args) {
+            /*
 		Pacman_Escape pac = new Pacman_Escape();
 		pac.setVisible(true);
 		pac.setTitle("Pacman");
 		pac.setSize(1280,720);
 		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pac.setLocationRelativeTo(null);
-		
+                
+		*/
+            Awal awal =new Awal();
+            awal.setVisible(true);
 	}
 }
