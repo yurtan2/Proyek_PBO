5369608a22ae7e7ec86b8e701c8d1b00dc6e89ec
MichaelSetiabudi <133090439+MichaelSetiabudi@users.noreply.github.com>
2023-06-03 13:24:02 +0700

    add pollymorph
    -mulai baris 41-45 class model
    -add class map1 yang berisi map

---
+++ Pacman_Escape/src/main/java/com/mycompany/pacman_escape/Map.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.pacman_escape;
+
+/**
+ *
+ * @author W I N D O W S
+ */
+public interface Map {
+    short[] getMapMaze();
+}
