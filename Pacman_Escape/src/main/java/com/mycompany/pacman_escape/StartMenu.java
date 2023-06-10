7c4a4c9cbe4bbfe7692367bb4ac332e886ff0c3c
Error404 <yogateguh68@gmail.com>
2023-06-10 17:07:51 +0700

    new start menu

--- Pacman_Escape/src/main/java/com/mycompany/pacman_escape/StartMenu.java
+++ Pacman_Escape/src/main/java/com/mycompany/pacman_escape/StartMenu.java
@@ -31,19 +31,23 @@ public class StartMenu extends javax.swing.JFrame {
         StartButton = new javax.swing.JButton();
         OptionButton = new javax.swing.JButton();
         ExitButton = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         jLabel1.setBackground(new java.awt.Color(0, 0, 153));
         jLabel1.setForeground(new java.awt.Color(51, 0, 204));
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 153));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setLocationByPlatform(true);
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
         jPanel1.setPreferredSize(new java.awt.Dimension(400, 400));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        StartButton.setBackground(new java.awt.Color(0, 204, 255));
+        StartButton.setBackground(new java.awt.Color(255, 255, 0));
         StartButton.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        StartButton.setForeground(new java.awt.Color(0, 102, 255));
         StartButton.setText("START");
         StartButton.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
         StartButton.addActionListener(new java.awt.event.ActionListener() {
@@ -51,9 +55,11 @@ public class StartMenu extends javax.swing.JFrame {
                 StartButtonActionPerformed(evt);
             }
         });
+        jPanel1.add(StartButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 280, 200, 80));
 
-        OptionButton.setBackground(new java.awt.Color(0, 204, 255));
+        OptionButton.setBackground(new java.awt.Color(255, 255, 0));
         OptionButton.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        OptionButton.setForeground(new java.awt.Color(0, 102, 255));
         OptionButton.setText("OPTION");
         OptionButton.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
         OptionButton.addActionListener(new java.awt.event.ActionListener() {
@@ -61,9 +67,12 @@ public class StartMenu extends javax.swing.JFrame {
                 OptionButtonActionPerformed(evt);
             }
         });
+        jPanel1.add(OptionButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 280, 200, 80));
+        OptionButton.getAccessibleContext().setAccessibleDescription("");
 
-        ExitButton.setBackground(new java.awt.Color(0, 204, 255));
+        ExitButton.setBackground(new java.awt.Color(255, 255, 0));
         ExitButton.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        ExitButton.setForeground(new java.awt.Color(0, 102, 255));
         ExitButton.setText("EXIT");
         ExitButton.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
         ExitButton.addActionListener(new java.awt.event.ActionListener() {
@@ -71,47 +80,23 @@ public class StartMenu extends javax.swing.JFrame {
                 ExitButtonActionPerformed(evt);
             }
         });
+        jPanel1.add(ExitButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 440, 220, 80));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(StartButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(100, 100, 100)
-                        .addComponent(OptionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(90, 90, 90)
-                        .addComponent(ExitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(StartButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(OptionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(90, 90, 90)
-                        .addComponent(ExitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        OptionButton.getAccessibleContext().setAccessibleDescription("");
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Yoga Pramana ST\\Documents\\GitHub\\Proyek_PBO\\Pacman_Escape\\src\\images\\pacmaze.jpg")); // NOI18N
+        jLabel3.setText("jLabel3");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -10, 1280, 740));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 484, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1280, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 444, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 720, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 26, Short.MAX_VALUE))
         );
 
         pack();
@@ -174,6 +159,7 @@ public class StartMenu extends javax.swing.JFrame {
     private javax.swing.JButton OptionButton;
     private javax.swing.JButton StartButton;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
